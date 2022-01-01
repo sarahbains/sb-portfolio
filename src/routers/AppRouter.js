bd735583a4943fa8134fbd9c9e185153ce64e38d
sarahbains <87668267+sarahbains@users.noreply.github.com>
2022-01-01 13:47:43 -0800

    Fixed Scroll Position

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,20 +4,23 @@ import Footer from '../components/Footer';
 import MainPage from '../pages/MainPage'
 import ProjectsPage from '../pages/ProjectsPage';
 import SingleProjectPage from '../pages/SingleProjectPage';
+import ScrollToTop from './ScrollToTop';
 import ParticlesComponant from '../components/ParticlesComponant';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop>
       {/* <ParticlesComponant/> */}
       <Header/>
-    <div>
-        <Route path="/" exact> <MainPage/> </Route>
-        <Route path="/all-projects" exact> <ProjectsPage/> </Route>
-        <Route path="/project/:slug"> <SingleProjectPage/> </Route>
-        <Route path="/project/:liveurl"> </Route>
-         <Footer />
-    </div>
+      <div>
+          <Route path="/" exact> <MainPage/> </Route>
+          <Route path="/all-projects" exact> <ProjectsPage/> </Route>
+          <Route path="/project/:slug"> <SingleProjectPage/> </Route>
+          <Route path="/project/:liveurl"> </Route>
+          <Footer />
+      </div>
+      </ScrollToTop>
     </Router>
   );
 }
